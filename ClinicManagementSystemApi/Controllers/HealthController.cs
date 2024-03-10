7c4b608d84b7ffd38776bcdf34da9b323204faf7
mkdad mahfoud <59840441+mekdad-dot@users.noreply.github.com>
2024-03-10 18:03:36 +0400

    -adding health check controller -adding dummy data controller -ignoring appsetings.json

---
+++ ClinicManagementSystemApi/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManagementSystemApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok();
+    }
+}
